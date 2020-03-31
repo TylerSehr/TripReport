fa7439a9c7766bd3310c29e3d23f5253f2fc5e4c
Jack Lenihan <jacklenihan300@gmail.com>
2020-03-30 21:41:04 -0500

    front end views

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Router} from '@reach/router'
+import MakeReport from './views/MakeReport';
+import ViewReports from './views/ViewReports';
+import ReadReport from './views/ReadReport';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ViewReports path='/reports'/>
+        <MakeReport path='/reports/compose'/>
+        <ReadReport path='/reports/:id'/>
+      </Router>
     </div>
   );
 }
